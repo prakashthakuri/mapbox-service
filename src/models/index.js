3be85c1818829947996cb0557f0e282ff61a4df8
Prakash Thakuri <prakashthakuri1994@gmail.com>
2024-07-23 22:18:54 -0500

    successfully database connected

--- src/models/index.js
+++ src/models/index.js
@@ -5,7 +5,15 @@ import PolygonModel from "./polygonModel.js";
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    // this is to fix pg_hba.conf
+    // https://stackoverflow.com/questions/25000183/node-js-postgresql-error-no-pg-hba-conf-entry-for-host
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+            }
+    }
 })
 
 const Polygon = PolygonModel(sequelize)
