caf9239977d617566cb75a754281e2041c86ec7b
Prakash Thakuri <prakashthakuri1994@gmail.com>
2024-07-23 23:32:20 -0500

    mutation fixes

--- src/resolvers/polygonResolver.js
+++ src/resolvers/polygonResolver.js
@@ -1,15 +1,21 @@
 import databaseConnection from "../models/index.js"
 
+const {Polygon} = databaseConnection
 export const polygonResolver = {
     Query: {
         getPolygons: async () => {
             console.log("getting polygons")
-            const polygons = await databaseConnection.Polygon.findAll()
+            const polygons = await Polygon.findAll()
             console.log(polygons, "data should be here")
             return polygons;
         }
     },
     Mutation: {
-
+        addPolygon: async (_, {input}) => {
+            console.log(input, "input")
+            const newPolygon = await Polygon.create(input)
+            console.log(newPolygon);
+            return newPolygon
+        }
     }
 }
