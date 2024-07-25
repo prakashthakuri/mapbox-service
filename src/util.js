2fe3a1cf4f7b5f599b5a1503dda0e1b2a1da9fa6
Prakash Thakuri <prakashthakuri1994@gmail.com>
2024-07-25 10:11:54 -0500

    polygon validation update

--- src/util.js
+++ src/util.js
@@ -1,37 +1,31 @@
 import { getLoggerInstance } from "./middlewares/logger.js";
-
+import * as turf from '@turf/turf';
 const logger= getLoggerInstance()
+
 export const validatePolygon = (coordinates) => {
-  if (!Array.isArray(coordinates) || coordinates.length === 0) {
-    logger.error("Invalid input. Coordinates must be a non-empty array.");
-    throw new Error("Invalid input. Coordinates must be a non-empty array.");
-  }
+  if (coordinates.length > 0) {
+    let lines = coordinates[0];
 
-  coordinates.forEach(ring => {
-    // if (!Array.isArray(ring) || ring.length < 4) {
-    //   logger.error("Invalid Polygon. Each ring of a polygon must have at least 4 coordinates.");
-    //   throw new Error("Invalid Polygon. Each ring of a polygon must have at least 4 coordinates.");
-    // }
-
-    // Ensure the first and last coordinates are the same to close the polygon
-    const firstCoord = ring[0];
-    const lastCoord = ring[ring.length - 1];
-    if (firstCoord[0] !== lastCoord[0] || firstCoord[1] !== lastCoord[1]) {
-      logger.error("Invalid Polygon. The first and last coordinates of each ring must be the same.");
-      throw new Error("Invalid Polygon. The first and last coordinates of each ring must be the same.");
+    // Remove the duplicate closing point if present
+    if (lines[0][0] === lines[lines.length - 1][0] && lines[0][1] === lines[lines.length - 1][1]) {
+      lines = lines.slice(0, -1);
     }
 
-    // Ensure all coordinates are valid numbers
-    // ring.forEach(coord => {
-    //   if (
-    //     !Array.isArray(coord) ||
-    //     coord.length !== 2 ||
-    //     typeof coord[0] !== 'number' ||
-    //     typeof coord[1] !== 'number'
-    //   ) {
-    //     logger.error("Invalid Polygon. Each coordinate must be an array of two numbers.");
-    //     throw new Error("Invalid Polygon. Each coordinate must be an array of two numbers.");
-    //   }
-    // });
-  });
-};
+    if (lines.length >= 3) {
+      const polygon = turf.polygon([lines.concat([lines[0]])]); // Ensure the polygon is closed by appending the first coordinate at the end
+      const isValid = turf.booleanValid(polygon);
+      logger.info('polygon is validated!')
+      return isValid;
+    } else {
+      logger.error("Invalid polygon.  A polygon must have at least 3 points.");
+
+      throw new Error('Invalid polygon. A polygon must have at least 3 points.');
+    }
+  } else {
+    logger.error("Invalid input. Coordinates must be a non-empty array.");
+
+    throw new Error('Invalid coordinates. Non-empty array expected.');
+
+  }
+}
+
